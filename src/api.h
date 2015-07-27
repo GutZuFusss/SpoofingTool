a0a1794ec7ca2fc09e7c5e634ecbf2556231f193
xushWT <sockenleidenschaft@gmail.com>
2015-07-27 11:05:50 +0100

    Paszczak's  fix

--- src/api.h
+++ src/api.h
@@ -4,15 +4,15 @@
 
 int PackSay(unsigned char *buffer, char *message, int team);
 
-int PackConnect(unsigned char *buffer);
-int PackClientInfo(unsigned char *buffer);
-int PackReady(unsigned char *buffer);
-int PackSendInfo(unsigned char *buffer);
-int PackEnterGame(unsigned char *buffer);
+int PackConnect(unsigned char *buffer, int id);
+int PackClientInfo(unsigned char *buffer, int id);
+int PackReady(unsigned char *buffer, int id);
+int PackSendInfo(unsigned char *buffer, int id);
+int PackEnterGame(unsigned char *buffer, int id);
 
-int PackKeepAlive(unsigned char *buffer);
+int PackKeepAlive(unsigned char *buffer, int id);
 
-void Reset();
+void Reset(int id);
 
 
 #endif
