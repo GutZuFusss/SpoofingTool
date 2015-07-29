18c772dad730abbc4a54ef4b01c1b71a029246ee
xushWT <sockenleidenschaft@gmail.com>
2015-07-29 10:16:19 +0200

    added little sleep to fetch ips so we can read the output

--- src/tools/mass_stats/fetchips.py
+++ src/tools/mass_stats/fetchips.py
@@ -1,4 +1,5 @@
 import urllib2
+import time
 
 response = urllib2.urlopen("https://stats.teeworlds.tw/last_refreshes.php?time=60")
 page_source = response.read()
@@ -9,3 +10,5 @@ print "Fetched IPs successfully!"
 f.close()
 num_lines = sum(1 for line in open('ips.txt'))
 print "Total IPs fetched: "+str(num_lines)
+
+time.sleep(5)
