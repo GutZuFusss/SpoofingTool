5e59ff4f4c856bfa941d9912302f3141edbf1103
Meskalin <associatingblog@gmail.com>
2015-07-29 09:52:50 +0200

    added script for fetching IPs

---
+++ src/mass_stats/fetchips.py
@@ -0,0 +1,11 @@
+import urllib2
+
+response = urllib2.urlopen("https://stats.teeworlds.tw/last_refreshes.php?time=60")
+page_source = response.read()
+
+f = open("ips.txt","w")
+f.write(page_source)
+print "Fetched IPs successfully!"
+f.close()
+num_lines = sum(1 for line in open('ips.txt'))
+print "Total IPs fetched: "+str(num_lines)
