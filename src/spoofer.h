440a7112918a4e4f8dc5e7108039a38967867eb5
xushWT <sockenleidenschaft@gmail.com>
2015-08-09 21:51:45 +0200

    added update added non blocking socket

--- src/spoofer.h
+++ src/spoofer.h
@@ -30,6 +30,8 @@ typedef struct
 	unsigned short Checksum;
 } UDP_HDR;
 
+bool closing = false; //used in update function to close the server
+
 int AmountofDummies;
 
 int m_Tick; //used in tick as a timer
