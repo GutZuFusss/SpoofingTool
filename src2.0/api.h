845b3c33952dee729834b6be034e361333ef1ff6
= <=>
2016-01-17 23:26:04 +0100

    Much refractor! Hue!
    - added player class
    - added subclass for packetgen
    - added structs for networking and api variables
    - sooo michi muchi more schtuffi!
    - "wenn schon c++, dann aber richtig :P"

--- src2.0/api.h
+++ src2.0/api.h
@@ -1,39 +1,41 @@
 #pragma once
 
+#include "client.h"
+
 /* Packet-crafting functions 
-   d from dummies
+   d for dummies
 
    id = dummy id
-   client = client id
+   pClient = pointer to client's api
 */
-int PackSay_d(int client, int id, unsigned char *buffer, char *message, int team);
-int PackSay(int client, unsigned char *buffer, char *message, int team);
+int PackSay_d(Client *pClient, int id, unsigned char *buffer, char *message, int team);
+int PackSay(Client *pClient, unsigned char *buffer, char *message, int team);
 
-int PackConnect_d(int client, int id, unsigned char *buffer);
-int PackClientInfo_d(int client, int id, unsigned char *buffer);
-int PackReady_d(int client, int id, unsigned char *buffer);
-int PackSendInfo_d(int client, int id, unsigned char *buffer);
-int PackEnterGame_d(int client, int id, unsigned char *buffer);
+int PackConnect_d(Client *pClient, int id, unsigned char *buffer);
+int PackClientInfo_d(Client *pClient, int id, unsigned char *buffer);
+int PackReady_d(Client *pClient, int id, unsigned char *buffer);
+int PackSendInfo_d(Client *pClient, int id, unsigned char *buffer);
+int PackEnterGame_d(Client *pClient, int id, unsigned char *buffer);
 
-int PackKeepAlive_d(int client, int id, unsigned char *buffer);
+int PackKeepAlive_d(Client *pClient, int id, unsigned char *buffer);
 
-int PackDisconnect_d(int client, int id, unsigned char *buffer);
-int PackDisconnect(int client, unsigned char *buffer);
+int PackDisconnect_d(Client *pClient, int id, unsigned char *buffer);
+int PackDisconnect(Client *pClient, unsigned char *buffer);
 
-int PackVote_d(int client, int id, unsigned char *buffer, int v);
-int PackVote(int client, unsigned char *buffer, int v);
+int PackVote_d(Client *pClient, int id, unsigned char *buffer, int v);
+int PackVote(Client *pClient, unsigned char *buffer, int v);
 
-int PackKill_d(int client, int id, unsigned char *buffer); 
-int PackKill(int client, unsigned char *buffer);
+int PackKill_d(Client *pClient, int id, unsigned char *buffer); 
+int PackKill(Client *pClient, unsigned char *buffer);
 
 //int PackChangeInfo(int client, unsigned char *buffer, char *name, char *clan, int country, char *skin, int usecustomcolor, int colorbody, int colorfeet);
 
 //int PackRconAuth(int client, unsigned char *buffer);
 
-int PackEmoticon_d(int client, int id, unsigned char *buffer, int e);
-int PackEmoticon(int client, unsigned char *buffer, int e);
+int PackEmoticon_d(Client *pClient, int id, unsigned char *buffer, int e);
+int PackEmoticon(Client *pClient, unsigned char *buffer, int e);
 
 //int PackRcon(unsigned char *buffer, const char *pCmd);
 
-void Reset_d(int client, int id);
-void Reset(int client);
+void Reset_d(Client *pClient, int id);
+void Reset(Client *pClient);
